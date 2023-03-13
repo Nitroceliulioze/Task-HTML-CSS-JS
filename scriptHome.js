d3c718923475a87fc88c30daf4c9167d300591d6
Nitroceliulioze <aksionova.diana@gmail.com>
2023-03-13 16:36:33 +0200

    add constructor methods

--- scriptHome.js
+++ scriptHome.js
@@ -1,67 +1,92 @@
 // HOME.html
 //MOVIE LIST 
 
-// class Movie {
-//     constructor(title, genre, price, stock) {
-//         this.title = title,
-//         this.genre = genre,
-//         this.price = price,
-//         this.stock = stock
-//     }
-// }
+class Movie {
+    constructor(title, genre, price, stock) {
+        this.title = title,
+            this.genre = genre,
+            this.price = price,
+            this.stock = stock
+    }
+    getTitle = () => {
+        return this.title
+    };
 
-// const batman = new Movie('Batman', 'Action', 4.55, 0);
-// const superman = new Movie('Superman', 'Action', 4.55, 3);
-// const aquaman = new Movie('Aquaman', 'Action', 4.55, 3);
-// const mermaid = new Movie('Little mermaid', 'Amination', 3.5, 5);
-// const aladdin = new Movie('Aladdin', 'Animation', 4, 4);
-// const titanic = new Movie('Titanic', 'Drama', 7, 10);
-// const starwars = new Movie('Star Wars', 'Action', 6, 12);
-// const got = new Movie('Game of Thrones', 'Fantasy', 12, 0);
+    getGenre = () => {
+        return this.genre
+    };
 
-const movieList = [{
-    title: 'Batman',
-    genre: 'Action',
-    price: 4.55,
-    stock: 0
-}, {
-    title: 'Superman',
-    genre: 'Action',
-    price: 4.55,
-    stock: 3
-}, {
-    title: 'Aquaman',
-    genre: 'Action',
-    price: 4.55,
-    stock: 21
-}, {
-    title: 'Little mermaid',
-    genre: 'Amination',
-    price: 3.5,
-    stock: 5
-}, {
-    title: 'Aladdin',
-    genre: 'Animation',
-    price: 4,
-    stock: 4
-}, {
-    title: 'Titanic',
-    genre: 'Drama',
-    price: 7,
-    stock: 10
-}, {
-    title: 'Star Wars',
-    genre: 'Action',
-    price: 6,
-    stock: 12
-}, {
-    title: 'Game of Thrones',
-    genre: 'Fantasy',
-    price: 12,
-    stock: 0
-}]
+    getPrice = () => {
+        return this.price
+    };
 
-// movieList.push(batman, superman, aquaman, mermaid, aladdin, titanic, starwars, got)
+    getStock = () => {
+        return this.stock
+    }
+
+    getMovies = () => {
+        return this.movies
+    }
+
+    addMovie = (movie) => {
+        this.movies.push(movie)
+    }
+
+}
+
+const batman = new Movie('Batman', 'Action', 4.55, 0);
+const superman = new Movie('Superman', 'Action', 4.55, 3);
+const aquaman = new Movie('Aquaman', 'Action', 4.55, 3);
+const mermaid = new Movie('Little mermaid', 'Amination', 3.5, 5);
+const aladdin = new Movie('Aladdin', 'Animation', 4, 4);
+const titanic = new Movie('Titanic', 'Drama', 7, 10);
+const starwars = new Movie('Star Wars', 'Action', 6, 12);
+const got = new Movie('Game of Thrones', 'Fantasy', 12, 0);
+
+// const movieList = [{
+//     title: 'Batman',
+//     genre: 'Action',
+//     price: 4.55,
+//     stock: 0
+// }, {
+//     title: 'Superman',
+//     genre: 'Action',
+//     price: 4.55,
+//     stock: 3
+// }, {
+//     title: 'Aquaman',
+//     genre: 'Action',
+//     price: 4.55,
+//     stock: 21
+// }, {
+//     title: 'Little mermaid',
+//     genre: 'Amination',
+//     price: 3.5,
+//     stock: 5
+// }, {
+//     title: 'Aladdin',
+//     genre: 'Animation',
+//     price: 4,
+//     stock: 4
+// }, {
+//     title: 'Titanic',
+//     genre: 'Drama',
+//     price: 7,
+//     stock: 10
+// }, {
+//     title: 'Star Wars',
+//     genre: 'Action',
+//     price: 6,
+//     stock: 12
+// }, {
+//     title: 'Game of Thrones',
+//     genre: 'Fantasy',
+//     price: 12,
+//     stock: 0
+// }]
+
+const movieList = []
+movieList.push(batman, superman, aquaman, mermaid, aladdin, titanic, starwars, got)
 
 
 const tableHomeMovies = document.getElementById("home-table")
